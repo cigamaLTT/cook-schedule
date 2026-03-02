303f4a5501a84215663c8d478e388c88886f4741
cigamaLTT <trunglcct@gmail.com>
2026-03-02 23:33:46 +0700

    update docker

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Update source code
-git pull origin main || git pull origin master
+# Update source code from master
+git pull origin master
 
 # Stop existing containers
-docker-compose down
+docker compose down
 
-# Rebuild and start containers in detached mode
-docker-compose up -d --build
+# Rebuild and start containers
+docker compose up -d --build
